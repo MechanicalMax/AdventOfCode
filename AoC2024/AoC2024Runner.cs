00efcbfe7f6aea755631364cdc1a1963f8c2a40f
Maximus Shurr <maxshurr@gmail.com>
2024-12-03 12:22:25 -0500

    Use DayRunner for AoC2024

---
+++ AoC2024/AoC2024Runner.cs
@@ -0,0 +1,18 @@
+﻿using AoCSupport;
+using System;
+
+namespace AoC2024
+{
+    internal class AoC2024Runner
+    {
+        public static void Main(string[] args)
+        {
+            Day day = new Day2();
+
+            // Choose day class from args?
+
+            DayRunner dayRunner = new DayRunner(day);
+            dayRunner.Run();
+        }
+    }
+}
