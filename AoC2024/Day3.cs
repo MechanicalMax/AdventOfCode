9e31060e69894eddae07141bfdf2cf4fd8831fe5
Maximus Shurr <maxshurr@gmail.com>
2024-12-03 12:49:48 -0500

    Solve Day 3 Part A

--- AoC2024/Day3.cs
+++ AoC2024/Day3.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC2024
+﻿using System.Text.RegularExpressions;
+
+namespace AoC2024
 {
     internal class Day3 : AoCSupport.Day
     {
@@ -6,7 +8,18 @@
         public override string Year => "2024";
         public override string PartA()
         {
-            return "";
+            Regex mulRegex = new Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+
+            int total = 0;
+
+            foreach (Match match in mulRegex.Matches(_input.Raw))
+            {
+                int numOne = int.Parse(match.Groups[1].Value);
+                int numTwo = int.Parse(match.Groups[2].Value);
+                total += numOne * numTwo;
+            }
+            
+            return total.ToString();
         }
         public override string PartB()
         {
