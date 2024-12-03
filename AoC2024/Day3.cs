6fa456e3a4d39c54d6eb31e39346ce31d470c4b8
Maximus Shurr <maxshurr@gmail.com>
2024-12-03 12:32:15 -0500

    Begin Day 3

---
+++ AoC2024/Day3.cs
@@ -0,0 +1,16 @@
+﻿namespace AoC2024
+{
+    internal class Day3 : AoCSupport.Day
+    {
+        public override string DayNumber => "3";
+        public override string Year => "2024";
+        public override string PartA()
+        {
+            return "";
+        }
+        public override string PartB()
+        {
+            return "";
+        }
+    }
+}
