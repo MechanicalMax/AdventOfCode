98fbd6a0095bb96045850135f32fa540a966ce87
Maximus Shurr <maxshurr@gmail.com>
2024-12-04 11:01:33 -0500

    Get Vertical Lines from Horizontal Lines

--- AoC2024/Day4.cs
+++ AoC2024/Day4.cs
@@ -11,9 +11,7 @@ namespace AoC2024
             Regex xmasPattern = new Regex("(?=XMAS|SAMX)");
 
             int total = 0;
-            foreach (string[] direction in new string[][] { 
-                _input.Lines,
-            })
+            foreach (string[] direction in GetAllDirections(_input.Lines))
             {
                 total += CountWordOccurrences(direction, xmasPattern);
             }
@@ -23,6 +21,26 @@ namespace AoC2024
         public override string PartB()
         {
             return "";
+        }
+        private string[] GetVerticalLines(string[] horizontal)
+        {
+            string[] vertical = new string[horizontal[0].Length];
+            for (int col = 0; col < horizontal[0].Length; col++)
+            {
+                string newRow = "";
+                for (int row = 0; row < horizontal.Length; row++)
+                {
+                    newRow += horizontal[row][col];
+                }
+                vertical[col] = newRow;
+            }
+            return vertical;
+        }
+        private string[][] GetAllDirections(string[] horizontal)
+        {
+            return new string[][] { horizontal, 
+                    GetVerticalLines(horizontal),
+                    };
         }
         private int CountWordOccurrences(string[] search, Regex word)
         {
