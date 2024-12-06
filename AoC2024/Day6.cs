f72ce4ae0ac49d6cafa32db96c80f45eb71def40
Maximus Shurr <maxshurr@gmail.com>
2024-12-06 11:06:25 -0500

    Solve Day 6

--- AoC2024/Day6.cs
+++ AoC2024/Day6.cs
@@ -8,6 +8,7 @@
         {
             public char[][] Layout { get; set; }
             public (int, int) GuardStart { get; init; }
+            public List<(int, int)> TraveledPoints { get; private set; }
             private (int, int)[] directions = [(-1, 0), (0, 1), (1, 0), (0, -1)];
             internal Grid(string[] lines)
             {
@@ -17,11 +18,12 @@
                     Layout[i] = lines[i].ToCharArray();
                 }
                 GuardStart = FindGuardStart();
+                TraveledPoints = new List<(int, int)>();
             }
             private (int, int) FindGuardStart()
             {
                 (int, int) guardStart = (-1, -1);
-
+                
                 bool foundStart = false;
                 for (int row = 0; row < this.Layout.Length; row++)
                 {
@@ -68,6 +70,7 @@
                 {
                     Layout[nextPosition.Item1][nextPosition.Item2] = '0';
                     curPosition = nextPosition;
+                    TraveledPoints.Add(curPosition);
                     nextPosition = (curPosition.Item1 + directions[curDirection].Item1,
                         curPosition.Item2 + directions[curDirection].Item2);
                 }
@@ -88,6 +91,40 @@
                 }
                 return count;
             }
+            public bool HasLoop()
+            {
+                // row, col, direction
+                var pastTurns = new List<(int, int, int)>();
+                var curPosition = GuardStart;
+                int curDirection = 0;
+                while (true)
+                {
+                    var nextPosition = (curPosition.Item1 + directions[curDirection].Item1,
+                        curPosition.Item2 + directions[curDirection].Item2);
+                    while (IsInGrid(nextPosition) && Layout[nextPosition.Item1][nextPosition.Item2] != '#')
+                    {
+                        curPosition = nextPosition;
+                        nextPosition = (curPosition.Item1 + directions[curDirection].Item1,
+                            curPosition.Item2 + directions[curDirection].Item2);
+                    }
+
+                    if (IsOnEdge(curPosition))
+                    {
+                        return false;
+                    }
+
+                    if (pastTurns.Contains((curPosition.Item1, curPosition.Item2, curDirection)))
+                    {
+                        Console.WriteLine("^Has Loop");
+                        DisplayLayout();
+                        return true;
+                    }
+                    pastTurns.Add((curPosition.Item1, curPosition.Item2, curDirection));
+
+                    curDirection++;
+                    curDirection %= 4;
+                }
+            }
         }
         public override string PartA()
         {
@@ -106,12 +143,50 @@
                 curDirection++;
                 curDirection %= 4;
             }
-            grid.DisplayLayout();
+            //grid.DisplayLayout();
             return grid.CountDistinctPositions().ToString();
         }
         public override string PartB()
         {
-            throw new NotImplementedException();
+            Grid grid = new Grid(_input.Lines);
+            int totalLoops = 0;
+
+            (int, int) curPosition = grid.GuardStart;
+            int curDirection = 0;
+
+            while (grid.IsInGrid(curPosition))
+            {
+                curPosition = grid.MarkTraveled(curPosition, curDirection);
+                if (grid.IsOnEdge(curPosition))
+                {
+                    break;
+                }
+                curDirection++;
+                curDirection %= 4;
+            }
+
+            var searchPoints = new List<(int, int)>();
+            for (int i = 0; i < grid.Layout.Length; i++)
+            {
+                for (int j = 0; j < grid.Layout[0].Length; j++)
+                {
+                    searchPoints.Add((i, j));
+                }
+            }
+
+            foreach ((int, int) traveledPoint in searchPoints)
+            {
+                char original = grid.Layout[traveledPoint.Item1][traveledPoint.Item2];
+                grid.Layout[traveledPoint.Item1][traveledPoint.Item2] = '#';
+
+                Console.WriteLine($"{traveledPoint.Item1} {traveledPoint.Item2}");
+
+                totalLoops += grid.HasLoop() ? 1 : 0;
+
+                grid.Layout[traveledPoint.Item1][traveledPoint.Item2] = original;
+            }
+
+            return totalLoops.ToString();
         }
     }
 }
