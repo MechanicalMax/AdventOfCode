cbc9e79d22b4be18d4f1560a272cad45588ce5a6
Maximus Shurr <maxshurr@gmail.com>
2024-12-12 12:51:27 -0500

    Start Day 12

--- AoC2024/AoC2024Runner.cs
+++ AoC2024/AoC2024Runner.cs
@@ -6,7 +6,7 @@ namespace AoC2024
     {
         public static void Main(string[] args)
         {
-            Day day = new Day11();
+            Day day = new Day12();
 
             // Choose day class from args?
 
---
+++ AoC2024/Day12.cs
@@ -0,0 +1,16 @@
+﻿namespace AoC2024
+{
+    internal class Day12 : AoCSupport.Day
+    {
+        public override string DayNumber => "12";
+        public override string Year => "2024";
+        public override string PartA()
+        {
+            throw new NotImplementedException();
+        }
+        public override string PartB()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
