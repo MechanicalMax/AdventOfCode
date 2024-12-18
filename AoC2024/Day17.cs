ed58e24f6bb29e37206d639f4964aea2b6e8ef67
Maximus Shurr <maxshurr@gmail.com>
2024-12-17 20:22:52 -0500

    Solve Day 17 With Help

--- AoC2024/Day17.cs
+++ AoC2024/Day17.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.Win32;
-
-namespace AoC2024
+﻿namespace AoC2024
 {
     internal class Day17 : AoCSupport.Day
     {
+        /*
+         * Part B: unable to solve due to time constraints
+         * Found solution from alexandrajay2002
+         * Reimplemented their part2 python solution in C# below
+         * https://pastebin.com/34vpnE8f
+         * https://tio.run/##lVbbbtswDH33V7AoBtuIF@TWoTCSAtuAPQ7DXlPXUGwlERZLhiRv6ddnlC3fs6w1kDiiyEMekqKSv@qj4MvLZS9FBupVActyITUQefjtOE5K95ATqWgs6YEpTaV3Ypz6oQP4MJ7SM2zAiKZ7XHlu6PowgUW5TXka1yrVewLzcufPkZ1oZ3@NGJRX0EB4WiJuy72w0YqmTKXswLRn3fd9TDYIXsol1YXk6FJ7V3H8HrFcioMk2Tt4WfytcXD2YS8knNGmG3Q0VfkJI3UD14@67jwlE@slDsD6Rlcork2e@TN3A5j7pVar0g/Xvv1uRFZm/R2obqtGAtgFkAQg8kSkDdG9XZsKLEOAeyBKFRkFfaT1FlOG3E4UPFVN3q3HSqWq9qlF22xgFQ51yTW1h5HazqopOtpKLDNNlTaMLN/AdAEyK3RVYEtuhzlL8DOr8tj@rLoPBWtj2HphXGlZJJoJHjQh1k62eRLmyWQRter7roUhM2uxzEPQmsDT0@1KOG0vn8aQ8z7kDl42ddJv2i2GdpvbUcAHeLyNuOwjogKBuxHnJtWdzug@ieCa8eI/0a@usE5umzz0TbAZ8kK/ifWAlbW8ayvfNFY05mo78xvBfh0GMBpM/4r907hY7@ub7lkps/xOBKwYRrnojc@WAEaI58SevYww3gyfsJ7zUA3wZiiV8nuYmQBUOUwYZ5qRE4rMfOcCMiGpnbbVUKNKIczWmwU9LPiIozDqHFyz2bHx4anbhEgyRbqEJ7S8dhq9aS5yb@Y3imZsJ8eC/zLTTRJ@oN6j308jp2cdm1PsEVivYWmugNLEGTYNF7qaSpXJcDTVEfnhGw6EhWxZXD1ktkqVv4FxhzTJc4zAa@JqUE1W/V7BvwuOUTjoOo45yWgcG9duHJuSx7Eb2vvIXHtVF5QXmvmnsJ1HPqJdLpefttngcwiL2WLlNIIvIY7fZvXVrJwfVZbwd7AMHoIVfs/@Ag
+         */
         public override string DayNumber => "17";
         public override string Year => "2024";
         public override string PartA()
@@ -56,80 +61,90 @@ namespace AoC2024
         }
         public override string PartB()
         {
-            (int rA, int rB, int rC, int[] instructions) = (2024, 0, 0, [0, 3, 5, 4, 3, 0]);
-            long registerA = rA;
-            long registerB = rB;
-            long registerC = rC;
+            (_, _, _, int[] instructions) = ParseDebuggerInput(_input.Lines);
 
-            registerA = 0;
-            long count = registerA;
-            
-            bool outputIsWrong = true;
-            
-            while (outputIsWrong)
+            var nextValidNumbers = new Queue<(long, int)>();
+            nextValidNumbers.Enqueue((0, instructions.Length - 1));
+
+            while (nextValidNumbers.Count > 0)
             {
-                //Console.WriteLine(registerA);
-                int outputIndex = 0;
-                bool hadBadOutput = false;
+                (long currentRegisterA, int distance) = nextValidNumbers.Dequeue();
 
-                int instructionPointer = 0;
-
-                while (!hadBadOutput && instructionPointer < instructions.Length)
+                for (int i = 0; i < 8; i++)
                 {
-                    int instruction = instructions[instructionPointer];
-                    int operand = instructions[instructionPointer + 1];
+                    long nextRegisterA = (currentRegisterA << 3) + i;
 
-                    switch (instruction)
+                    (bool correctOutput, int outputLength) = ProvidesCorrectOutput(instructions, distance, nextRegisterA, 0, 0);
+
+                    if (!correctOutput)
                     {
-                        case 0:
-                            registerA = registerA / (int)Math.Pow(2, GetCombo(operand, registerA, registerB, registerC));
-                            break;
-                        case 1:
-                            registerB ^= (long)operand;
-                            break;
-                        case 2:
-                            registerB = GetCombo(operand, registerA, registerB, registerC) % 8;
-                            break;
-                        case 3:
-                            if (registerA != 0)
-                            {
-                                instructionPointer = operand - 2;
-                            }
-                            break;
-                        case 4:
-                            registerB ^= registerC;
-                            break;
-                        case 5:
-                            long newOutput = GetCombo(operand, registerA, registerB, registerC) % 8;
-                            if (outputIndex >= instructions.Length || instructions[outputIndex] != newOutput)
-                            {
-                                hadBadOutput = true;
-                            }
-                            else
-                            {
-                                outputIndex++;
-                            }
-                            break;
-                        case 6:
-                            registerB = registerA / (int)Math.Pow(2, GetCombo(operand, registerA, registerB, registerC));
-                            break;
-                        case 7:
-                            registerC = registerA / (int)Math.Pow(2, GetCombo(operand, registerA, registerB, registerC));
-                            break;
+                        continue;
                     }
-                    instructionPointer += 2;
-                }
-                if (hadBadOutput == false && outputIndex == instructions.Length)
-                {
-                    outputIsWrong = false;
-                }
-                else
-                {
-                    registerA = ++count;
+
+                    if (distance == 0)
+                    {
+                        return nextRegisterA.ToString();
+                    }
+
+                    nextValidNumbers.Enqueue((nextRegisterA, distance - 1));
                 }
             }
+            return "Not Found";
+        }
+        private (bool, int) ProvidesCorrectOutput(int[] instructions, int distance, long registerA, long registerB, long registerC)
+        {
+            int outputIndex = distance;
+            bool hadBadOutput = false;
 
-            return count.ToString();
+            int instructionPointer = 0;
+
+            while (!hadBadOutput && instructionPointer < instructions.Length)
+            {
+                int instruction = instructions[instructionPointer];
+                int operand = instructions[instructionPointer + 1];
+
+                switch (instruction)
+                {
+                    case 0:
+                        registerA = registerA / (int)Math.Pow(2, GetCombo(operand, registerA, registerB, registerC));
+                        break;
+                    case 1:
+                        registerB ^= (long)operand;
+                        break;
+                    case 2:
+                        registerB = GetCombo(operand, registerA, registerB, registerC) % 8;
+                        break;
+                    case 3:
+                        if (registerA != 0)
+                        {
+                            instructionPointer = operand - 2;
+                        }
+                        break;
+                    case 4:
+                        registerB ^= registerC;
+                        break;
+                    case 5:
+                        long newOutput = GetCombo(operand, registerA, registerB, registerC) % 8;
+                        if (outputIndex >= instructions.Length || instructions[outputIndex] != newOutput)
+                        {
+                            hadBadOutput = true;
+                        }
+                        else
+                        {
+                            outputIndex++;
+                        }
+                        break;
+                    case 6:
+                        registerB = registerA / (int)Math.Pow(2, GetCombo(operand, registerA, registerB, registerC));
+                        break;
+                    case 7:
+                        registerC = registerA / (int)Math.Pow(2, GetCombo(operand, registerA, registerB, registerC));
+                        break;
+                }
+                instructionPointer += 2;
+            }
+
+            return (!hadBadOutput, outputIndex);
         }
         private long GetCombo(int instruction, long registerA, long registerB, long registerC)
         {
