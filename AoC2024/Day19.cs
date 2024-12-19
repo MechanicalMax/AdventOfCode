6354e31152153fab60f8d2d0f8b1e489b983c5ee
Maximus Shurr <maxshurr@gmail.com>
2024-12-19 11:53:01 -0500

    Solve Part A

--- AoC2024/Day19.cs
+++ AoC2024/Day19.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC2024
+﻿using System.Text.RegularExpressions;
+
+namespace AoC2024
 {
     internal class Day19 : AoCSupport.Day
     {
@@ -6,11 +8,28 @@
         public override string Year => "2024";
         public override string PartA()
         {
-            throw new NotImplementedException();
+            (string[] patterns, string[] designs) = ParsePatternsAndDesigns(_input.Lines);
+
+            string regexString = $"^({string.Join('|',patterns)})+$";
+            var patternMatcher = new Regex(regexString);
+
+            int matches = 0;
+            foreach (var design in designs)
+            {
+                if (patternMatcher.IsMatch(design)) {
+                    matches++;
+                }
+            }
+
+            return matches.ToString();
         }
         public override string PartB()
         {
             throw new NotImplementedException();
+        }
+        private (string[], string[]) ParsePatternsAndDesigns(string[] lines)
+        {
+            return (lines[0].Split(", "), new ArraySegment<string>(lines, 2, lines.Length - 2).ToArray());
         }
     }
 }
