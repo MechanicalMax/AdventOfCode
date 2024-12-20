dfe53913026d6f7ff6ce284fc42b3b14b3514598
Maximus Shurr <maxshurr@gmail.com>
2024-12-20 07:53:20 -0500

    Start Day 20

--- AoC2024/AoC2024Runner.cs
+++ AoC2024/AoC2024Runner.cs
@@ -6,7 +6,7 @@ namespace AoC2024
     {
         public static void Main(string[] args)
         {
-            Day day = new Day19();
+            Day day = new Day20();
 
             // Choose day class from args?
 
