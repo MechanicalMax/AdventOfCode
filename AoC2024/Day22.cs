addcd7b5690524688193308eeb533ead6721fb34
Maximus Shurr <maxshurr@gmail.com>
2024-12-22 13:37:58 -0500

    Solve Day 22 Part A

--- AoC2024/Day22.cs
+++ AoC2024/Day22.cs
@@ -6,11 +6,39 @@
         public override string Year => "2024";
         public override string PartA()
         {
-            throw new NotImplementedException();
+            long sum = 0;
+
+            foreach (string start in _input.Lines)
+            {
+                long curResult = long.Parse(start);
+                for (int _ = 0; _ < 2000; _++)
+                {
+                    curResult = evolveNumber(curResult);
+                }
+                sum += curResult;
+            }
+
+            return sum.ToString();
         }
         public override string PartB()
         {
             throw new NotImplementedException();
+        }
+        private long evolveNumber(long secretNumber)
+        {
+            long stepOne = secretNumber << 6;
+            secretNumber ^= stepOne;
+            secretNumber %= 16777216;
+            //secretNumber = secretNumber >> 24;
+            
+            long stepTwo = secretNumber >> 5;
+            secretNumber ^= stepTwo;
+            secretNumber %= 16777216;
+
+            long stepThree = secretNumber << 11;
+            secretNumber ^= stepThree;
+            secretNumber %= 16777216;
+            return secretNumber;
         }
     }
 }
